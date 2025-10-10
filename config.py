8ef0086ca535f0888fb6b7123a9ce6fd0ea4e9e0
Pavel Ryzhkov <81442161+povilas1565@users.noreply.github.com>
2025-10-10 18:24:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 ﻿import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7601278255:AAFFbnzm3fZEcOPfmJ2tbsXFHnv1h4YfYFI")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8209248067:AAGjMQeIbnLHG3zUhKOL2g4luB8if-Ig4m4")
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:root@localhost:5432/myrentcar")
 FREEKASSA_MERCHANT_ID = os.getenv("FREEKASSA_MERCHANT_ID", "your_fk_merchant_id")
 FREEKASSA_SECRET_1 = os.getenv("FREEKASSA_SECRET_1", "your_fk_secret_word_1")  # для создания ссылок
